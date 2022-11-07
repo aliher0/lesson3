fab876121088538021822139f335e69d062287db
aliher0 <alisertolgoev434@gmail.com>
2022-11-07 15:43:06 +0300

    lesson3

---
+++ main.js
@@ -0,0 +1,75 @@
+// Операторы(унарные = бинарные = условные )
+// var age = 19
+// var num = 10 //  не (наборот)
+
+var user = {
+    userName: 'aliher',
+    surname:  'tolgoev',
+    age: 13,
+    Traffic_light: {
+        type: prompt('Введите тип свет'),
+        
+        
+        
+    }
+}
+switch (user.Traffic_light.type) {
+    case 'кызыл':
+        console.log('токто!');
+        break;
+        case 'жашыл':
+            console.log('журо бер')
+            break;
+        case 'сары':
+            console.log('даярдан')
+            break;
+        default:
+            console.log('мындай свет жок'); 
+            break;    
+}
+
+
+// var nav = prompt
+// console.log(nav 'САН ЖАЗЫНЫЗ')
+
+// console.log(++num)
+// if (age > 18) {
+//     console.log('ЭКИНЧИ САН ЧОН')
+// }
+// else if (age == 18) {
+//     console.log('БИРИНЧИ САН ЧОН')
+
+// }
+// else {
+//     console.log('меньше 18')
+// }
+// бинарные операторы
+//num = num + 10
+// num += 10
+// num -= 10
+// num /= 10 
+//  num *= 10
+//  num **= 10
+//  console.log(num)
+
+// var boolAnd = true && true // и - жана экоонун бироосу false ,болуш керек
+// var boolAnd = true || true // или - жээ экоонун бироосу true болсо true болот
+// var boolAnd =  true ^ true // и экоотен бир значениеде болуш керек
+
+
+
+//операторы сравные == ===
+
+//console.log(5 !== '5')
+
+
+
+
+//console.log(num + 10)
+//console.log(num - 10)
+//console.log(num * 5)
+//console.log(num / 2)
+//console.log(num2 % 5)//1
+// 100 000
+
+// операторы 
